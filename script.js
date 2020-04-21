b7a21801421dba7fa1f274d00a268d6b3fafaeef
Dzmitry Liashchou <liashchou.d@gmail.com>
2020-04-21 11:12:03 +0200

    Fourth ex Part 3

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function start() {
     money = +prompt("Your monthly budget?"); // Plus для того чтобы перевести тип в число
     time = prompt("Input date in format YYYY-MM-DD");
 
-    while (isNaN(money) || money == '' || money == null) { //money == null для того чтобы пользователь не нажал кнопку отмена
-        money = +prompt("Your monthly budget?");
+    while (isNaN(money) || money == '' || money == null) { //money==null для того чтобы пользователь 
+        money = +prompt("Your monthly budget?"); //не нажал кнопку отмена
     }
 }
 
@@ -19,79 +19,95 @@ let appData = {
     expensis: {},
     optional: {},
     income: [],
-    savings: true
+    savings: true,
+
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt("How much money do you have to spend?", ''),
+                b = prompt("How much will iy cost?", '');
+
+            if ((typeof (a) === 'string') && (typeof (a)) != null &&
+                (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+                console.log("done");
+                appData.expensis[a] = b; //Когда пользователь ответит на вопрос "а" мы получим ответ "b" (ключ-значение)
+            } else {
+                console.log("Error");
+                i = i - 1;
+            }
+        }
+    },
+
+    detectDayBudget: function () { // Расчет дневного бюджета
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+        alert("Every day budget: " + appData.moneyPerDay);
+    },
+
+    detectLevel: function () { // Расчет уровня достатка
+        if (appData.moneyPerDay < 100) {
+            console.log("Minimal level of money");
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log("Middle level of money");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("You are rich!");
+        } else {
+            console.log("Error!");
+        }
+    },
+
+    checkSavings: function () { // Функция для расчета дохода с нашего депозита
+        if (appData.savings == true) {
+            let save = +prompt("What is the amount of savings"),
+                percent = +prompt("At what percentage?");
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert("Income from your deposit?" + appData.monthIncome);
+        }
+    },
+
+    chooseOptExpenses: function () { // Функция для определения необязательных расходов
+        for (let i = 0; i < 3; i++) {
+            let optExp = prompt("Optional expenses?");
+
+            appData.optionalExpenses[i] = "1 : " + optExp;
+
+            console.log(appData.optionalExpenses);
+        }
+    },
+
+    chooseIncome: function () {
+        let items = prompt("what will bring additional income? (Input wia ,)", '');
+        while (!isNaN(items) || items == '' || items == null) {
+            prompt("what will bring additional income? (Input wia ,)", '');
+        }
+        appData.income = items.split(", ");
+        appData.income.push(prompt("Maybe something else?"));
+        appData.income.sort();
+
+        this.income.forEach(function (item, i) {
+            alert("Ways of bringing additional incomes: " + (i++) + ". " + " - " + item);
+        });
+    },
+
+
+
 };
 
-//appData.expensis[firstq1] = firstq2;
-//appData.expensis[secondq1] = secondq2;
-
-
-
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("How much money do you have to spend?", ''),
-            b = prompt("How much will iy cost?", '');
-
-        if ((typeof (a) === 'string') && (typeof (a)) != null &&
-            (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
-            console.log("done");
-            appData.expensis[a] = b; //Когда пользователь ответит на вопрос "а" мы получим ответ "b" (ключ - значение)
-        } else {
-            console.log("Error");
-            i = i - 1;
-        }
-    }
+for (let key in appData) {
+    console.log("Our program is include: " + key + " => " + appData[key]);
 }
 
-chooseExpenses();
 
-// Расчет дневного бюджета
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
 
-    alert("Every day budget: " + appData.moneyPerDay);
-}
 
-detectDayBudget();
 
-// Расчет уровня достатка
-function detectLevel() {
-    if (appData.moneyPerDay < 100) {
-        console.log("Minimal level of money");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log("Middle level of money");
-    } else if (appData.moneyPerDay > 2000) {
-        console.log("You are rich!");
-    } else {
-        console.log("Error!");
-    }
-}
 
-detectLevel();
 
-// Функция для определения необязательных расходов
-function chooseOptExpenses() {
-    for (let i = 0; i < 3; i++) {
-        let optExp = prompt("Optional expenses?");
 
-        appData.optionalExpenses[i] = "1 : " + optExp;
 
-        console.log(appData.optionalExpenses);
-    }
-}
 
-// Функция для расчета дохода с нашего депозита
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("What is the amount of savings"),
-            percent = +prompt("At what percentage?");
 
-        appData.monthIncome = save / 100 / 12 * percent;
-        alert("Income from your deposit?" + appData.monthIncome);
-    }
-}
 
-checkSavings();
 
 // Используем цикл WHILE
 
